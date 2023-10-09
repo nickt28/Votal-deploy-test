a3aecba701b5cb4c974faf6976e20973e7bac1e9
Nick T <93458816+nickt28@users.noreply.github.com>
2023-10-09 11:29:29 +1100

    Added Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="bg-gray-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <Footer />

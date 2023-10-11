06ad14e2727e74aab9ec4382fb47ecd9e1f4a7b3
Nick T <93458816+nickt28@users.noreply.github.com>
2023-10-11 21:01:20 +1100

    Tailwind obfuscator

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import twObfuscator from 'tailwind-obfuscator';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	// preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess({}),
+		twObfuscator({
+		  enable: true, // Set to true to enable class obfuscation (default is true)
+		}),
+	],
 };
 
 export default config;
